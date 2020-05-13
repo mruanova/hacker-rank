4f78cdf002b69543c161aa66586c5e1fc15d0eeb
Mauricio Ruanova-Hurtado <mruanova@gmail.com>
2020-05-12 20:18:25 -0500

    news

--- app.js
+++ app.js
@@ -1,14 +1,53 @@
 const http = require('http');
-
 const hostname = '127.0.0.1';
 const port = 3000;
-
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
 });
-
+const getHackerRankNews = () => {
+    const url = 'hacker-news.firebaseio.com';
+    const path = '/v0/item/8863.json?print=pretty';
+    const options = {
+        hostname: url,
+        port: 443,
+        path,
+        method: 'GET'
+    };
+    const body = [];
+    const executor = ((resolve, reject) => {
+        const handleError = (error) => {
+            console.error(error);
+            reject(error.message);
+        };
+        const https = require('https');
+        https.request(options, res => {
+            const listener = (chunk) => {
+                body.push(chunk); // process.stdout.write(d)
+            };
+            const handleEnd = () => {
+                const temp = JSON.parse(Buffer.concat(body).toString());
+                resolve(temp);
+            };
+            res.on('data', listener)
+                .on('end', handleEnd);
+        })
+            .on('error', handleError)
+            .end();
+    });
+    const promise = new Promise(executor);
+    return promise;
+};
+(async () => {
+    try {
+        const response = await getHackerRankNews();
+        const temp = JSON.stringify(response.descendants);
+        console.log('getHackerRankNews', temp);
+    } catch (error) {
+        console.log('catch.error', error);
+    }
+})();
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
